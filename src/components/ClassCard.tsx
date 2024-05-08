d5b68266fb66e951246365d3ed72d7a948ade806
Bruno <rbrunog21@gmail.com>
2024-05-08 19:23:49 -0300

    refactor: add ClassCard component

---
+++ src/components/ClassCard.tsx
@@ -0,0 +1,12 @@
+export function ClassCard() {
+    return (
+        <div className="h-full bg-[#272727] min-w-36 max-h-36 rounded-lg px-1 py-3 text-center">
+            <p className="text-white text-[0.65rem]">
+            Análise e Desenvolvimento de Sistemas
+            </p>
+            <p className="text-white text-[0.65rem] mb-8">(5º semestre)</p>
+            <p className="text-[0.65rem] text-gray-400">Laboratório 4</p>
+            <p className="text-[0.65rem] text-gray-400">18:45 - 19:35</p>
+        </div>
+    )
+}
